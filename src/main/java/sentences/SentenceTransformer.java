04d426cff66a1a4ea5ad6a955351f32cb2387bc0
SzabolcsMarton <maszabolcs@gmail.com>
2022-02-28 12:54:55 +0100

    refactor SentenceTransformer

--- src/main/java/sentences/SentenceTransformer.java
+++ src/main/java/sentences/SentenceTransformer.java
@@ -5,17 +5,25 @@ public class SentenceTransformer {
     private final static String END_OF_SENTENCE = ".!?";
 
     public String shortenSentence(String sentence) {
-         if(!Character.isUpperCase(sentence.charAt(0))){
-            throw new IllegalArgumentException( "Must start with capital letter!");
-         }
-         if(END_OF_SENTENCE.indexOf(sentence.charAt(sentence.length()-1)) == -1){
-             throw new IllegalArgumentException("Must end with . ! or ?");
-         }
-         String[] words = sentence.split(" ");
-         if(words.length < 5){
-             return sentence;
-         }
-        return words[0] + " ... " + words[words.length-1];
+        ensureStartsWithCapital(sentence);
+        ensureEndSign(sentence);
+        String[] words = sentence.split(" ");
+        if (words.length < 5) {
+            return sentence;
+        }
 
+        return words[0] + " ... " + words[words.length - 1];
+    }
+
+    private void ensureEndSign(String sentence) {
+        if (END_OF_SENTENCE.indexOf(sentence.charAt(sentence.length() - 1)) == -1) {
+            throw new IllegalArgumentException("Must end with . ! or ?");
+        }
+    }
+
+    private void ensureStartsWithCapital(String sentence) {
+        if (!Character.isUpperCase(sentence.charAt(0))) {
+            throw new IllegalArgumentException("Must start with capital letter!");
+        }
     }
 }
